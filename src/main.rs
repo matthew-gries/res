1731490cc1d5af1d47c1d76296ba64174b6a53c4
matthew-gries <gries.m@husky.neu.edu>
2020-08-11 00:07:36 -0400

    Started system code

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,37 @@ mod memory;
 mod instruction;
 mod system;
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+const BUFFER_SIZE: usize = 524288;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Please supplt ROM path.");
+    }
+
+    let rom_path = Path::new(&args[1]);
+    let mut file = File::create(rom_path)?;
+
+    let mut buf: [u8; BUFFER_SIZE+16] = [0; BUFFER_SIZE+16];
+
+    file.read(&mut buf)?;
+
+    let mut mem = memory::Memory::new();
+    // load rom into program memory
+    for i in 0..BUFFER_SIZE {
+        mem.write((i + 0x8000) as u16, buf[16+i]);
+    }
+
+    let mut cpu = cpu::CPU::new();
+    cpu.init();
+    let mut system = system::NesSystem::new(cpu, mem);
+
+    system.start();
+
+    Ok(())
 }
--- src/system.rs
+++ src/system.rs
@@ -5,3 +5,16 @@ pub struct NesSystem {
     cpu: CPU,
     memory: Memory
 }
+
+impl NesSystem {
+
+    pub fn new(cpu: CPU, memory: Memory) -> Self {
+        NesSystem{cpu, memory}
+    }
+
+    pub fn start(&mut self) {
+        loop {
+            self.cpu.instruction_cycle(&mut self.memory).unwrap();
+        }
+    }
+}
