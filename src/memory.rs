0724c0243006fa84d82d90dea29f0d4f14102ab2
matthew-gries <gries.m@husky.neu.edu>
2020-08-15 14:24:22 -0400

    implemented memory mirroring

--- src/memory.rs
+++ src/memory.rs
@@ -5,11 +5,15 @@ pub struct Memory {
     data: [u8; MEMORY_MAP_ADDRESSABLE_RANGE]
 }
 
-impl Memory {
+enum MemoryMapSegments {
+    RAM(u16),
+    IORegisters(Option<u16>),
+    ExpansionRom,
+    SRAM,
+    PRGROM,
+}
 
-    pub fn new() -> Self {
-        Memory{data: [0; MEMORY_MAP_ADDRESSABLE_RANGE]}
-    }
+impl Memory {
 
     pub fn from_data_buffer(data: [u8; MEMORY_MAP_ADDRESSABLE_RANGE]) -> Self {
         Memory{data}
@@ -22,15 +26,53 @@ impl Memory {
         addr1_high != addr2_high
     }
 
+    fn get_mem_map_segment(addr: u16) -> MemoryMapSegments {
+	if addr < 0x2000 {
+	    MemoryMapSegments::RAM(0x0800)
+	} else if addr < 0x4000 {
+	    MemoryMapSegments::IORegisters(Some(0x0008))
+	} else if addr < 0x4020 {
+	    MemoryMapSegments::IORegisters(None)
+	} else if addr < 0x6000 {
+	    MemoryMapSegments::ExpansionRom
+	} else if addr < 0x8000 {
+	    MemoryMapSegments::SRAM
+	} else {
+	    MemoryMapSegments::PRGROM
+	}
+    }
+
+    fn get_address(addr: u16, segment: MemoryMapSegments) -> u16 {
+	
+	match segment {
+	    MemoryMapSegments::RAM(mask) => addr % mask,
+	    MemoryMapSegments::IORegisters(optional_mask) => {
+		if let Some(mask) = optional_mask {
+		    (addr % mask) + 0x2000
+		} else {
+		    addr
+		}
+	    },
+	    MemoryMapSegments::ExpansionRom
+		| MemoryMapSegments::SRAM
+		| MemoryMapSegments::PRGROM
+		=> addr
+	}
+    }
+
     pub fn read(&self, addr: u16) -> u8 {
-        self.data[addr as usize]
+	let mem_map_segment = Self::get_mem_map_segment(addr);
+	let adjusted_addr = Self::get_address(addr, mem_map_segment);
+        self.data[adjusted_addr as usize]
     }
 
     pub fn write(&mut self, addr: u16, byte: u8) {
-        if addr >= 0x8000 {
-            panic!("Attempted to write into ROM at address {}!", addr);
-        }
 
-        self.data[addr as usize] = byte
+	let mem_map_segment = Self::get_mem_map_segment(addr);
+	let adjusted_addr = match mem_map_segment {
+	    MemoryMapSegments::ExpansionRom | MemoryMapSegments::PRGROM => panic!("Attempted to write to Read Only memory!"),
+	    _ => Self::get_address(addr, mem_map_segment)
+	};
+        self.data[adjusted_addr as usize] = byte
     }
 }
